f0c95ebd026148e1a931ae6873f3a94514083f2d
kistenklaus <karlsasssie@gmail.com>
2023-11-07 18:37:11 +0100

    change config refs to Arcs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{rc::Rc, sync::Arc};
 
 use can_config_rs::config::{Network, NetworkBuilder};
 use errors::Result;
@@ -7,13 +7,13 @@ mod errors;
 mod parser;
 
 
-pub fn parse_yaml_config_from_file(path : &str) -> Result<Rc<Network>> {
+pub fn parse_yaml_config_from_file(path : &str) -> Result<Arc<Network>> {
     let src = std::fs::read_to_string(path)?;
     let network = parse_yaml_config(&src)?;
     Ok(network)
 }
 
-pub fn parse_yaml_config(src : &str) -> Result<Rc<Network>> {
+pub fn parse_yaml_config(src : &str) -> Result<Arc<Network>> {
     let mut network_builder = NetworkBuilder::new();
 
     let docs = yaml_rust::yaml::YamlLoader::load_from_str(src)?;
